756e9c1fdf194247e405c6b2f3421d8821694491
kaobianzhan <3192340469@qq.com>
2025-02-26 09:00:45 +0800

    Site updated: 2025-02-26 09:00:41

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 const CountdownTimer = (() => {
     const config = {
-        targetDate: "2025-06-01",
-        targetName: "儿童节",
+        targetDate: "2026-02-17",
+        targetName: "春节",
     };
 
     function updateCountdown() {
